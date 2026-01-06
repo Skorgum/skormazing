7402b3988f0704bada2feca7fa49782a1c7586d1
Skorgum <skorgum@gmail.com>
2026-01-05 19:45:54 -0500

    Add testing

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 from graphics import Line, Point
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -20,6 +20,9 @@ class Cell:
         self.__x2 = x2
         self.__y2 = y2
 
+        if self.__win is None:
+            return
+
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self.__win.draw_line(line)
@@ -34,13 +37,16 @@ class Cell:
             self.__win.draw_line(line)
 
     def draw_move(self, to_cell, undo=False):
-        half_lenth = abs(self.__x2 - self.__x1) // 2
-        x_center = half_lenth + self.__x1
-        y_center = half_lenth + self.__y1
+        half_length = abs(self.__x2 - self.__x1) // 2
+        x_center = half_length + self.__x1
+        y_center = half_length + self.__y1
 
-        half_lenth2 = abs(to_cell.__x2 - to_cell.__x1) // 2
-        x_center2 = half_lenth2 + to_cell.__x1
-        y_center2 = half_lenth2 + to_cell.__y1
+        half_length2 = abs(to_cell.__x2 - to_cell.__x1) // 2
+        x_center2 = half_length2 + to_cell.__x1
+        y_center2 = half_length2 + to_cell.__y1
+
+        if self.__win is None:
+            return
 
         fill_color = "red"
         if undo:
