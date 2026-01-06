7e8afcf33aafa42053ca8487b67c9f8939827a2d
Skorgum <skorgum@gmail.com>
2026-01-05 20:24:44 -0500

    Add solving logic

--- cell.py
+++ cell.py
@@ -1,14 +1,16 @@
 from graphics import Line, Point
 
+
 class Cell:
     def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self.__x1 = -1
-        self.__y1 = -1
         self.__x2 = -1
+        self.__y1 = -1
         self.__y2 = -1
         self.__win = win
 
@@ -16,25 +18,33 @@ class Cell:
         if self.__win is None:
             return
         self.__x1 = x1
-        self.__y1 = y1
         self.__x2 = x2
+        self.__y1 = y1
         self.__y2 = y2
-
-        if self.__win is None:
-            return
-
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self.__win.draw_line(line)
-        if self.has_right_wall:
-            line = Line(Point(x2, y1), Point(x2, y2))
-            self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.__win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.__win.draw_line(line, "white")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.__win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.__win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.__win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self.__x2 - self.__x1) // 2
@@ -44,9 +54,6 @@ class Cell:
         half_length2 = abs(to_cell.__x2 - to_cell.__x1) // 2
         x_center2 = half_length2 + to_cell.__x1
         y_center2 = half_length2 + to_cell.__y1
-
-        if self.__win is None:
-            return
 
         fill_color = "red"
         if undo:
--- graphics.py
+++ graphics.py
@@ -1,11 +1,12 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
 
@@ -17,7 +18,7 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
-        print("Window closed...")
+        print("window closed...")
 
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
@@ -25,16 +26,18 @@ class Window:
     def close(self):
         self.__running = False
 
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+
 class Line:
     def __init__(
         self,
         p1,
-        p2
+        p2,
     ):
         self.p1 = p1
         self.p2 = p2
@@ -43,4 +46,3 @@ class Line:
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
-
